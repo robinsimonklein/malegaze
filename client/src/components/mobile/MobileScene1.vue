25bff75c2c9dc4f6085d6e66f9491906fc18e027
Robin Simonklein <rsimonklein@gmail.com>
2020-04-18 15:50:41 +0200

    MobileControls

--- client/src/components/mobile/MobileScene1.vue
+++ client/src/components/mobile/MobileScene1.vue
@@ -2,19 +2,21 @@
     <div class="mobile-scene-1">
         <h1>Mobile Scène 1</h1>
         <button @click="next" class="btn">Next</button>
+        <ZoomSlider class="mobile-scene-1__zoom" />
 
         <!-- Track the mobile orientation -->
-        <MobileOrientation :debug="true" />
+        <MobileOrientation :debug="false" />
     </div>
 </template>
 
 <script>
     import appStates from "../../js/appStates";
     import MobileOrientation from "./orientation/MobileOrientation";
+    import ZoomSlider from "./controls/ZoomSlider";
 
     export default {
         name: "MobileScene1",
-        components: { MobileOrientation },
+        components: {ZoomSlider, MobileOrientation },
         methods: {
             next() {
                 this.$socket.emit('state_request', appStates.SCENE2)
@@ -23,6 +25,10 @@
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .mobile-scene-1 {
+        &__zoom {
+            margin: 1rem 0;
+        }
+    }
 </style>
---
+++ client/src/components/mobile/controls/ZoomSlider.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="zoom-slider">
+        <input v-model="focalLength" type="range" min="24" max="150" step="0.1" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ZoomSlider",
+        data() {
+            return {
+                focalLength: 24
+            }
+        },
+        methods: {
+            emitControls() {
+                this.$socket.emit('mobile_controls', {focalLength: this.focalLength})
+            }
+        },
+        watch: {
+            focalLength() {
+                this.emitControls()
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.zoom-slider {
+
+}
+</style>
