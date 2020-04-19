0be216aded8fafed553404df1e0deb02647d0e2e
Robin Simonklein <rsimonklein@gmail.com>
2020-04-19 20:26:01 +0200

    Import video in view

--- client/src/components/desktop/DesktopIntro.vue
+++ client/src/components/desktop/DesktopIntro.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="desktop-intro">
         <h1>Desktop Intro</h1>
+        <video>
+            <source src="@/assets/video/intro.mp4" type="video/mp4">
+        </video>
     </div>
 </template>
 
