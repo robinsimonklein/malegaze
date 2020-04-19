e67291cdbc74814c53453e77788dfff417c0e9c9
Robin Simonklein <rsimonklein@gmail.com>
2020-04-19 21:29:23 +0200

    Play video + go next state when ended

--- client/src/components/desktop/DesktopIntro.vue
+++ client/src/components/desktop/DesktopIntro.vue
@@ -1,18 +1,50 @@
 <template>
     <div class="desktop-intro">
-        <h1>Desktop Intro</h1>
-        <video>
+        <button class="desktop-intro__start" @click="startVideo">Forcer la vidéo (provisoir)</button>
+        <video class="desktop-intro__video" ref="player">
             <source src="@/assets/video/intro.mp4" type="video/mp4">
         </video>
     </div>
 </template>
 
 <script>
+    import appStates from "../../js/appStates";
+
     export default {
         name: "DesktopIntro",
+        data() {
+            return {
+                player: null
+            }
+        },
+        methods: {
+            startVideo() {
+                this.$refs.player.play()
+            }
+        },
+        mounted() {
+            // Commencer la vidéo si on arrive directement sur cette vue, provisoir.
+            this.$refs.player.addEventListener('ended', () => {
+                this.$store.dispatch('app/requestState', appStates.SCENE1)
+            })
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+.desktop-intro {
+    &__start {
+        position: fixed;
+        top: 1rem;
+        left: 1rem;
+        z-index: 2;
+        color: black;
+        background-color: white;
 
+    }
+    &__video {
+        width: 100vw;
+        height: 100vh;
+    }
+}
 </style>
