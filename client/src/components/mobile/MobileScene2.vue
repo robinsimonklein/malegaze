ed09c5eedb53870d9c113ba641519a7e110b6379
Robin Simonklein <rsimonklein@gmail.com>
2020-04-20 10:51:30 +0200

    Add mobile orientation stream for Scene 2

--- client/src/components/mobile/MobileScene2.vue
+++ client/src/components/mobile/MobileScene2.vue
@@ -2,14 +2,19 @@
     <div class="mobile-scene-2">
         <h1>Mobile Scène 2</h1>
         <button @click="next" class="btn">Next</button>
+
+        <!-- Track the mobile orientation -->
+        <MobileOrientation :debug="false" />
     </div>
 </template>
 
 <script>
     import appStates from "../../js/appStates";
+    import MobileOrientation from "./orientation/MobileOrientation";
 
     export default {
         name: "MobileScene2",
+        components: {MobileOrientation},
         methods: {
             next() {
                 this.$socket.emit('state_request', appStates.SCENE3)
