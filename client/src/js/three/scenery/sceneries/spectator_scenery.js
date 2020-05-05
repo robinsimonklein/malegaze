d29643fab60d65838c46ce7ede32997690612b27
sonia.rouabhi <sonia.rouabhi@labonneagence.com>
2020-05-05 22:49:34 +0200

    Volumetric Lights

--- client/src/js/three/scenery/sceneries/spectator_scenery.js
+++ client/src/js/three/scenery/sceneries/spectator_scenery.js
@@ -5,18 +5,19 @@ import Light from '../../light/Light';
 import cameraTypes from '../../camera/cameraTypes';
 import controlsTypes from '../../controls/controlsTypes';
 import * as THREE from 'three';
+import THREEx from '../../light/VolumetricLight';
+import Sound from '../../sound/Sound';
 import store from '../../../../store';
 import appStates from '../../../appStates';
 
-export default new Scenery({
+export default new Scenery({ // TODO: Overlay
     name: 'spectator_scenery',
     orbitControls: null,
     cameras: [
         new Camera({
             type: cameraTypes.PERSPECTIVE,
             properties: {fov: 60, aspectRatio: window.innerWidth / window.innerHeight, near: 1, far: 8000},
-            initialPosition: {x: -20, y: 200, z: 520},
-            // initialPosition: {x: -200, y: 3000, z: -400},
+            initialPosition: {x: -20, y: 200, z: 520}, // TODO: Change position
         }),
     ],
     controls: controlsTypes.MOBILE,
@@ -37,32 +38,54 @@ export default new Scenery({
             light: new THREE.SpotLight(0xff4444),
             initialPosition: {x: 0, y: 100, z: -780},
             properties: {
-                castShadow: true
+                castShadow: true,
+                penumbra: 0.3
             }
+        }),
+    ],
+    sounds: [
+        new Sound({ // TODO: Son du lampadaire, son des voix
+            name: 'ambiance',
+            path: 'sound/ambianceScene3.mp3',
+            isLoop: true,
+            volume: 0.4,
         }),
     ],
     onCreated: (self) => {
         self.eyes = [];
         self.video = null;
         self.time = 0;
+        self.volumetricLights = [];
 
         /**
-         *
          * @param {*} self
-         * @param {string }name
-         * @param {number} x
-         * @param {number} y
-         * @param {number} z
-         * @return {Light}
+         * @param position
          */
-        self.buildLight = (self, name, [x, y, z]) => {
-            const pointLight = new THREE.PointLight(0xffffff);
+        self.buildVolumetricLight = (self, position) => { // TODO: Animer les lampadaires
+            const lightColor = 0xffaa44;
+            const geometry = new THREE.CylinderGeometry(25., 200., 340, 32 * 2, 20, true);
 
-            return new Light({
-               name: name,
-               light: pointLight,
-               initialPosition: {x, y, z}
-            });
+            geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 55, 0));
+            geometry.applyMatrix4(new THREE.Matrix4().makeRotationX(-0.15));
+
+            const material = new THREEx.VolumetricSpotLightMaterial(4.5, 15., position, new THREE.Color(lightColor), 1.);
+            const mesh = new THREE.Mesh(geometry, material);
+            mesh.position.set(position.x, position.y, position.z);
+            mesh.lookAt(new THREE.Vector3(0, 0, 0));
+            self.scene.add(mesh);
+            self.volumetricLights.push(mesh);
+
+            const spotLight = new THREE.SpotLight();
+            spotLight.position.set(position.x, position.y, position.z);
+            spotLight.color = new THREE.Color(lightColor);
+            spotLight.exponent = 30;
+            spotLight.angle = 0.9;
+            spotLight.intensity = 1;
+            spotLight.decay = 0.5;
+            spotLight.penumbra = 0.5;
+            spotLight.target.position.set(position.x, 0, position.z);
+            self.scene.add(spotLight);
+            self.scene.add(spotLight.target);
         }
 
         /**
@@ -70,7 +93,7 @@ export default new Scenery({
          * @param {*} self
          * @param {number[]} position
          */
-         self.createActress = (self, {position}) => {
+        self.createActress = (self, {position}) => {
             const objects = [];
 
             const sphereGeometry = new THREE.SphereGeometry(20, 100, 100);
@@ -86,6 +109,7 @@ export default new Scenery({
 
             for (let i = 0; i < 3; i++) {
                 const eye = new THREE.Mesh(sphereGeometry, material);
+                eye.castShadow = true;
                 switch (i) {
                     case 0:
                         eye.position.x = 50;
@@ -105,6 +129,12 @@ export default new Scenery({
             self.eyes.push(objects);
         }
 
+        /* TODO
+
+        self.buildGhost = (self) => {
+
+        } */
+
         /**
          * Build video
          * @param {*} self
@@ -118,7 +148,6 @@ export default new Scenery({
             source.setAttribute('type', 'video/mp4');
             self.video.appendChild(source);
             document.body.appendChild(self.video);
-            console.log(self.video);
         }
 
         /**
@@ -126,7 +155,7 @@ export default new Scenery({
          * @param {*} self
          */
         self.buildScreen = (self) => {
-            const geometry = new THREE.BoxGeometry(615, 305, 10);
+            const geometry = new THREE.BoxGeometry(615, 330, 10);
 
             const videoTexture = new THREE.VideoTexture(self.video);
             videoTexture.minFilter = THREE.LinearFilter;
@@ -143,7 +172,7 @@ export default new Scenery({
 
             const mesh = new THREE.Mesh(geometry, material);
             mesh.position.x = 0;
-            mesh.position.y = 270;
+            mesh.position.y = 268;
             mesh.position.z = -785;
 
             self.scene.add(mesh);
@@ -162,7 +191,9 @@ export default new Scenery({
         spotlight.shadow.camera.far = 4000;
         spotlight.shadow.camera.fov = 30;
 
-        self.lightManager.lightObjects.push(self.buildLight(self, 'test', [0, 200, -500]));
+        self.soundManager.addToCamera(self.cameraManager.camera);
+        window.addEventListener('keypress', () => self.soundManager.sound.play());
+        // self.soundManager.sound.play();
 
         // Create actors
         self.createActress(self, {position: [850, 270]});
@@ -183,10 +214,21 @@ export default new Scenery({
                 item.rotation.y += 0.02;
             });
         });
-        if (self.time > 1500) {
-            self.video.pause();
-            store.dispatch('app/requestState', appStates.END);
+
+        switch (self.time) {
+            case 1500:
+                self.buildVolumetricLight(self, new THREE.Vector3(860, 95, 200));
+                break;
+            case 2000:
+                self.buildVolumetricLight(self, new THREE.Vector3(-875, 95, 210));
+                break;
+            case 4000:
+                self.video.pause();
+                store.dispatch('app/requestState', appStates.END);
+                break;
         }
-        self.time++;
+        if (self.time < 4000) {
+            self.time++;
+        }
     }
 })
