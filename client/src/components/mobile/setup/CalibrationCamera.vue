bc5fc813d7be8063ef2f26a041d363b669a832f2
sonia.rouabhi <sonia.rouabhi@labonneagence.com>
2020-05-17 17:24:22 +0200

    calibration camera button

--- client/src/components/mobile/setup/CalibrationCamera.vue
+++ client/src/components/mobile/setup/CalibrationCamera.vue
@@ -250,14 +250,15 @@
             button {
                 opacity: 0;
                 text-transform: uppercase;
-                color: #FF4040;
+                color: #202020;
                 border: 1px solid #FF4040;
-                background: transparent;
+                background: #FF4040;
                 border-radius: 0;
                 width: 50vw;
                 left: calc(50% - 25vw);
                 bottom: 10%;
                 position: absolute;
+                font-weight: bold;
                 padding: 15px 0;
                 letter-spacing: 5px;
             }
