d04af3899e113003405dd323d7dd3a2f54a8f9ab
NicoJupiter <nicolas.jupiter@pistil-studio.fr>
2020-05-18 00:03:22 +0200

    add mobile intro screen

--- client/src/components/mobile/MobileIntro.vue
+++ client/src/components/mobile/MobileIntro.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="mobile-intro">
-        <h1> Mobile Intro </h1>
+        <div class="mobile-intro__wrap">
+            <img src="@/assets/png/mobile_intro_camera.png" alt="Camera image">
+            <h1> Projection en cours </h1>
+        </div>
+        <div class="mobile-intro__text">
+            Ne verouillez pas <br/> votre téléphone
+        </div>
         <!-- <button @click="next" class="btn">Next</button> -->
     </div>
 </template>
@@ -18,6 +24,36 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .mobile-intro {
 
+        font-family: "Roboto Mono", sans-serif;
+        text-transform: uppercase;
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        &__wrap {
+
+            margin: 0 auto;
+            img {
+                transform: translateX(12%);
+                margin-bottom: 25px;
+            }
+
+            h1 {
+                font-size: 15px;
+                margin-bottom: 74px;
+                text-align: center;
+            }
+        }
+
+        &__text {
+            font-size: 15px;
+            color: #FF4040;
+            text-align: center;
+        }
+
+    }
 </style>
