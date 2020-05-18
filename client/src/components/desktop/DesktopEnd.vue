60774c0991a3a09b6389e3ba651326d0e88cc08a
Robin Simonklein <rsimonklein@gmail.com>
2020-05-18 04:47:40 +0200

    End screens animation

--- client/src/components/desktop/DesktopEnd.vue
+++ client/src/components/desktop/DesktopEnd.vue
@@ -1,12 +1,12 @@
 <template>
     <div v-if="currentView === 'end'" class="desktop-end">
         <div class="definition">
-            <h1>Male Gaze</h1>
-            <p>Terme qui désigne le fait que la culture visuelle dominante impose au public d’adopter un regard d’homme hétérosexuel.</p>
+            <h1 class="definition__title">Male Gaze</h1>
+            <p class="definition__text">Terme qui désigne le fait que la culture visuelle dominante impose au public d’adopter un regard d’homme hétérosexuel.</p>
         </div>
-        <footer>
-            <img src="@/assets/png/goDownphone.png" alt="goDown"/>
-            <span>Le male gaze en 3 minutes</span>
+        <footer class="footer">
+            <img class="footer__icon" src="@/assets/png/goDownphone.png" alt="goDown"/>
+            <span class="footer__text">Le male gaze en 3 minutes</span>
         </footer>
     </div>
     <div v-else-if="currentView === 'credit'" class="desktop-end">
@@ -16,6 +16,8 @@
 
 <script>
     import DesktopCredit from './DesktopCredit';
+    import gsap from 'gsap';
+
     export default {
         name: "DesktopEnd",
         components: {DesktopCredit},
@@ -26,8 +28,22 @@
         },
         sockets: {
             mobile_scrolled() {
-                this.currentView = 'credit';
+                const tl = new gsap.timeline();
+                tl.to('.footer__icon', {duration: 1, y: -20, alpha: 0, ease: 'power1.in'})
+                tl.to('.footer__text', {duration: 1, y: -20, alpha: 0, ease: 'power1.in'}, '<0.2')
+                tl.to('.definition__title', {duration: 1, alpha: 0, y: -20, ease: 'power2.in'}, '<')
+                tl.to('.definition__text', {duration: 1, alpha: 0, y: -20, ease: 'power2.in'}, '<0.1')
+                tl.call(() => {
+                    this.currentView = 'credit';
+                }, [], '>2')
             }
+        },
+        mounted() {
+            const tl = new gsap.timeline()
+            tl.from('.definition__title', {duration: 3, delay: 1, alpha: 0, ease: 'power2.out'})
+            tl.from('.definition__text', {duration: 3, alpha: 0, ease: 'power2.out'}, '<0.5')
+            tl.from('.footer__text', {duration: 3, y: -20, alpha: 0, ease: 'power2.out'})
+            tl.from('.footer__icon', {duration: 3, y: -20, alpha: 0, ease: 'power2.out'}, '<0.5')
         }
     }
 </script>
--- client/src/components/mobile/MobileEnd.vue
+++ client/src/components/mobile/MobileEnd.vue
@@ -14,6 +14,7 @@
     import MobileEndImpact from "./end/MobileEndImpact";
     import MobileEndBetchdel from "./end/MobileEndBetchdel";
     import MobileEndConclusion from "./end/MobileEndConclusion";
+    import gsap from 'gsap'
 
     export default {
         name: "MobileEnd",
@@ -31,6 +32,8 @@
                 this.$socket.emit('mobile_scrolled');
                 this.isEmitted = true;
             }, false);
+
+            gsap.fromTo('.mobile-end', {alpha: 0}, {alpha: 1, duration: 5, delay: 3.5, ease: 'power3.out'})
         }
     }
 </script>
